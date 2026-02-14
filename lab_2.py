b823df9afdacc000c1e10a315f39cf952de8ce59
Abhi <ashaji@isi.edu>
2026-02-13 17:11:52 -0800

    added

--- lab_2.py
+++ lab_2.py
@@ -17,22 +17,20 @@ def most_sold_item(record_file_name: str) -> Tuple[str, int]:
     :return: tuple of (item_name, total_sold)
     """
     sales = {}
-    with open(record_file_name,"r") as f:
+    with open(record_file_name, "r") as f:
         for line in f:
-            parts = line.strip().split(',')
-            item_name,qty = parts[0],parts[2] # item_name, color, quantity
-            if qty<0:
-                sales[item_name] = sales.get(item_name,0)+abs(qty)
-    
-    if not sales :
-        return ("",0)
+            parts = line.strip().split(", ")
+            item_name, qty = parts[0], int(parts[2])
+            if qty < 0:
+                sales[item_name] = sales.get(item_name, 0) + abs(qty)
+
+    if not sales:
+        return ("", 0)
     
     best_name = ""
     best_sold = 0
-
-    for name,sold in sales.items():
+    for name, sold in sales.items():
         if sold > best_sold or (sold == best_sold and name < best_name):
             best_name = name
             best_sold = sold
-
-    return (best_name,best_sold)
+    return (best_name, best_sold)
